32735ee922d3731803c96c037ce9df6f52c316bd
A7medking1 <A7medking2011@icloud.com>
2025-12-26 18:51:59 +0200

    feat: add ScrollingActivity with layout and navigation components

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,54 +9,14 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
-    val firstFragment = OneFragment()
-    val secondFragment = TwoFragment()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         enableEdgeToEdge()
         setContentView(binding.root)
-        initSubView()
-        addClick()
     }
 
-    private fun addClick() {
-        binding.btn.setOnClickListener {
-            showSecondFragment()
-        }
-
-        binding.rmBtn.setOnClickListener {
-            removeFragment(secondFragment)
-        }
-    }
-
-    private fun removeFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.remove( fragment)
-        transaction.commit()
-    }
-
-    private fun showSecondFragment() {
-   //     addFragment(secondFragment)
-        replaceFragment(secondFragment)
-    }
-
-    private fun initSubView() {
-        addFragment(firstFragment)
-    }
-
-    private fun addFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragmentContainerView, fragment)
-        transaction.commit()
-    }
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmentContainerView, fragment)
-        transaction.commit()
-    }
 
 }
