bd412a72c76aef42ec22c1611823f4bafe8795e8
A7medking1 <A7medking2011@icloud.com>
2025-12-27 14:20:03 +0200

    feat: update MainActivity layout with user input fields and login functionality

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.myapplication
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,14 +15,50 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 //        enableEdgeToEdge()
         setContentView(binding.root)
+        initForms()
 
-        onClick()
+        onTapButton()
     }
 
-    private fun onClick () {
-        binding.fab.setOnClickListener {
-            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+
+    private fun onTapButton() {
+        binding.butonLogin.setOnClickListener {
+            onClick()
         }
+    }
+
+    private fun onClick() {
+        val sharedPrefs: SharedPreferences = getSharedPreferences("test", MODE_PRIVATE)
+        val edit = sharedPrefs.edit()
+
+        val userName = binding.userName.editText?.text.toString()
+        val password = binding.password.editText?.text.toString()
+
+        edit.putString("userName", userName)
+        edit.putString("password", password)
+
+        edit.apply()
+
+        Toast.makeText(this, "Login Successful! Welcome $userName", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun initForms() {
+        val sharedPrefs: SharedPreferences = getSharedPreferences("test", MODE_PRIVATE)
+
+        val userName = sharedPrefs.getString("userName", "")
+        val password = sharedPrefs.getString("password", "")
+
+        if (!userName.isNullOrEmpty()) {
+            binding.userName.editText?.setText(userName)
+        }
+        if (!password.isNullOrEmpty()) {
+            binding.password.editText?.setText(password)
+        }
+
+
+
+
     }
 
 
