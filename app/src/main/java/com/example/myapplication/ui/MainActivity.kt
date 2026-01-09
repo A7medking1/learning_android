a96052e5b97338ad67a7005602ecf05d49ff1891
A7medking1 <A7medking2011@icloud.com>
2026-01-10 01:33:55 +0200

    data binding

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.example.myapplication.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.viewModel.MainViewModel
+import androidx.activity.viewModels
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-
+    private val TAG = "MainActivity"
+    
+    // Data Binding instance
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?): Unit {
         super.onCreate(savedInstanceState)
+        
+        // Initialize data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-            setup()
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        setup()
     }
 
 
@@ -28,16 +37,16 @@ class MainActivity : AppCompatActivity() {
             viewModel.fetchAWisdom()
         }
 
-        viewModel.currentUser.observe(this) {
-            binding.userName.text = it.name
-        }
+//        viewModel.currentUser.observe(this) {
+//            binding.userName.text = it.name
+//        }
 
-        viewModel.currentWisdom.observe(this) {
-            binding.apply {
-                textDate.text = it.publishDate
-                content.text = it.content
-            }
-        }
+//        viewModel.currentWisdom.observe(this) {
+//            binding.apply {
+//                textDate.text = it.publishDate
+//                content.text = it.content
+//            }
+//        }
     }
 
 
